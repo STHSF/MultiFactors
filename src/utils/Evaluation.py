6be1d69b47652f55c51c585f60994389257d5d06
li <liyu52577@163.com>
2019-11-18 17:16:24 +0800

    code

---
+++ src/utils/Evaluation.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0
+@author: li
+@file: Evaluation.py
+@time: 2019/11/18 5:07 下午
+"""
+import numpy as np
+from sklearn import metrics
+
+
+class RegressionEvaluate(object):
+
+    @staticmethod
+    def mape(y_true, y_pred):
+        return np.mean(np.abs((y_pred - y_true) / y_true)) * 100
+
+    @staticmethod
+    def smape(y_true, y_pred):
+        return 2.0 * np.mean(np.abs(y_pred - y_true) / (np.abs(y_pred) + np.abs(y_true))) * 100
+
+    @staticmethod
+    def mse(y_true, y_pred):
+        return metrics.mean_squared_error(y_true, y_pred)
+
+    @staticmethod
+    def rmse(y_true, y_pred):
+        return np.sqrt(metrics.mean_squared_error(y_true, y_pred))
+
+    @staticmethod
+    def mae(y_true, y_pred):
+        return metrics.mean_absolute_error(y_true, y_pred)
+
+
+if __name__ == '__main__':
+    y_true = np.array([1.0, 5.0, 4.0, 3.0, 2.0, 5.0, -3.0])
+    y_pred = np.array([1.0, 4.5, 3.5, 5.0, 8.0, 4.5, 1.0])
+
+    eva = RegressionEvaluate()
+    # MSE
+    print(eva.mse(y_true, y_pred))  # 8.107142857142858
+    # RMSE
+    print(eva.rmse(y_true, y_pred))  # 2.847304489713536
+    # MAE
+    print(eva.mae(y_true, y_pred))  # 1.9285714285714286
+    # MAPE
+    print(eva.mape(y_true, y_pred))  # 76.07142857142858
+    # SMAPE
+    print(eva.smape(y_true, y_pred))  # 57.76942355889724
