fda26d5d2e69300a04d79cdfff8ec7c487b4d54d
li <liyu52577@163.com>
2019-11-20 15:42:44 +0800

    code

--- src/conf/configuration.py
+++ src/conf/configuration.py
@@ -46,10 +46,10 @@ class RegressionConfig(object):
             'feature_fraction': 0.9,  # 建树的特征选择比例 # 样本列采样
             'bagging_fraction': 0.8,  # 建树的样本采样比例
             'bagging_freq': 5,  # k 意味着每 k 次迭代执行bagging
-            'lambda_l1': 0,  # L1 正则化
-            'lambda_l2': 0,  # L2 正则化
+            'lambda_l1': 0.90,  # L1 正则化
+            'lambda_l2': 0.95,  # L2 正则化
             'bagging_seed': 100,  # 随机种子,light中默认为100
-            # 'verbose': 1  # <0 显示致命的, =0 显示错误 (警告), >0 显示信息
+            'verbose': 1  # <0 显示致命的, =0 显示错误 (警告), >0 显示信息
         }
         self.max_round = 500
         self.cv_folds = None
@@ -80,8 +80,8 @@ class ClassificationConfig(object):
                        'feature_fraction': 0.9,  # bagging_fraction相当于样本特征采样，使bagging运行更快的同时可以降拟合
                        'bagging_fraction': 0.95,  # 用来进行特征的子抽样，可以用来防止过拟合并提高训练速度[0.5, 0.6, 0.7,0.8,0.9]
                        'bagging_freq': 5,
-                       'lambda_l1': 0,
-                       'lambda_l2': 0.5,  # L2正则化系数
+                       'lambda_l1': 0.9,
+                       'lambda_l2': 0.95,  # L2正则化系数
                        # 'device': 'gpu',  # 默认使用集显
                        # 'gpu_platform_id': 1,  # 确定是使用集成显卡还是独立显卡，0代表独显，1代表独显
                        # 'gpu_device_id': 0  # id为0的独显
