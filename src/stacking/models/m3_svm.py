743ed636c8a03409c2e05607270972a64606ce15
li <liyu52577@163.com>
2019-11-21 19:23:44 +0800

    code

--- src/stacking/models/m3_svm.py
+++ src/stacking/models/m3_svm.py
@@ -12,5 +12,5 @@ from src.stacking.models.libsvm.python.svmutil import *
 
 y, x = svm_read_problem('./libsvm/heart_scale')
 
-m = svm_train(y[:200], x[:200], '-t 2, -c 4')
+m = svm_train(y[:200], x[:200], '-t 2 -c 4')
 p_label, p_acc, p_val = svm_predict(y[200:], x[200:], m)
