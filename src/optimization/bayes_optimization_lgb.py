54951d0aeb45744775f2ef813a5af604f42605f0
li <liyu52577@163.com>
2019-12-19 09:09:38 +0800

    glg6

--- src/optimization/bayes_optimization_lgb.py
+++ src/optimization/bayes_optimization_lgb.py
@@ -160,8 +160,9 @@ class BayesOptimizationLGBM(BayesOptimizationBase):
 
         best_round = best_model.best_iteration
         # 不同的metric可能有不同的best_score类型，使用时需要注意。
+        score = self.params['metric'][0]
         log.logger.info('best_score: \n{}'.format(best_model.best_score))
-        best_score = best_model.best_score['training']['l2']
+        best_score = best_model.best_score['training'][score]
         log.logger.info('parameters: \n{}'.format(self.params))
         log.logger.info(' Stopped after %d iterations with train-score = %f ' % (best_round, best_score))
         if best_score < self.BestScore:
